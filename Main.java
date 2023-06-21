dc6feafe811f7c63d82e914a82229f80035732d2
SlideForSlice <kenigilya9@gmail.com>
2023-06-21 08:41:26 +0500

    Initital commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        // подтотавливаем данные:
+
+        long amount = 100;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+
+        long actual = service.calculate(amount, registered);
+
+        // Производим проверку (сравниваем ожидаемый и фактический):
+        // Если true - PASS
+        // Если false - FALL
+
+        boolean passed = expected == actual;
+
+        // выводим результат
+
+        System.out.println(passed);
+    }
+}
